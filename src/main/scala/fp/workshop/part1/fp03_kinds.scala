67a57ad975444a1defeb400d10b4642abe771139
Lukasz Byczynski <l.byczynski@adform.com>
2018-08-16 12:33:45 +0200

    Part3 - Add existential part

--- src/main/scala/fp/workshop/part1/fp03_kinds.scala
+++ src/main/scala/fp/workshop/part1/fp03_kinds.scala
@@ -1,22 +1,30 @@
 package fp.workshop.part1
 
+import scala.language.existentials
+
 object fp03_kinds {
 
   // Type    Kind
   // Int     *
   // Option  * => *
-  // List    [*] => *
   // Either  [*, *] => *
+
+  // Scala does not have a kind annotation that looks like this: def foo[F: * => *, A: *](a: A).
+  // All types default to kind of * (star), so we use this:
+  def foo[F[_], A](fa: F[A]): F[A] = ???
+  def bar[F[_, _], A, B](fa: F[A, B]): F[A, B] = ???
+
   trait Sized[F[_]] {
     def size[A](fa: F[A]): Int
   }
 
-  val SizedList: Sized[List] = new Sized[List] {
-    override def size[A](fa: List[A]): Int = ???
-  }
+  lazy val SizedList: Sized[List] = ???
 
   // Partial application of the Map type, so that its kind turns from [*, *] => * into * => *
-//  def SizedMap[K]: Sized[Map[K, _]] = ???
+  type MapT[K] = Map[K, Any]
+  lazy val SizedMap: Sized[MapT] = ???
 
-//  def SizedTuple2[A]: Sized[(A, _)] = ???
+  // we can use existential type
+  type MapE[K] = Map[K,  T forSome { type T}]
+  lazy val SizedMapE: Sized[MapE] = ???
 }
--- src/test/scala/fp/workshop/part1/fp03_kindsTest.scala
+++ src/test/scala/fp/workshop/part1/fp03_kindsTest.scala
@@ -6,14 +6,16 @@ class fp03_kindsTest extends FunSuite {
   import fp03_kinds._
 
   test("list size") {
-    assert(SizedList.size(1 :: 2 :: 3 :: Nil) == 3)
+    assert(SizedList.size(List(1 :: 2 :: 3 :: Nil)) == 3)
   }
 
   test("map size") {
-//    assert(SizedMap.size(Map(1 -> 2, 2 -> 3)) == 2)
+    assert(SizedMap.size(Map(1 -> 2,  3 ->  4)) == 2)
+    assert(SizedMap.size(Map(1 -> "a",  3 ->  "a")) == 2)
   }
 
-  test("tuple size") {
-//    assert(SizedTuple2.size(("ss", "aaa")) == 1)
+  test("mapE size") {
+    assert(SizedMapE.size(Map(1 -> 2,  3 ->  4)) == 2)
   }
+
 }
