429bcfccfb309630fff38302072d416180707923
Lukasz Byczynski <l.byczynski@adform.com>
2018-10-02 22:44:45 +0200

    Update codebase to current http4s api

--- src/main/scala/fp/workshop/part2/ServerMain.scala
+++ src/main/scala/fp/workshop/part2/ServerMain.scala
@@ -1,23 +1,29 @@
 package fp.workshop.part2
 
-import cats.effect.IO.timer
-import cats.effect.{Effect, IO, _}
-import fs2.StreamApp
+import cats.effect._
 import org.http4s.HttpRoutes
 import org.http4s.server.blaze.BlazeBuilder
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContext.Implicits.global
 
-object ServerMain extends StreamApp[IO] {
-  override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = ServerStream.stream[IO]
+object ServerMain extends IOApp {
+
+  private implicit val ec: ExecutionContext = ExecutionContext.global
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    ServerStream.stream[IO].compile.last.map {
+      case Some(ExitCode.Success) => ExitCode.Success
+      case _ => ExitCode.Error
+    }
+  }
 }
 
 object ServerStream {
-  def jsonService[F[_]: Effect]: HttpRoutes[F] = new services.DummyService[F].jsonService
 
+  def jsonService[F[_] : Effect]: HttpRoutes[F] =
+    new services.DummyService[F].jsonService
 
-  def stream[F[_]: ConcurrentEffect](implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
+  def stream[F[_] : ConcurrentEffect](implicit ec: ExecutionContext): fs2.Stream[F, ExitCode] = {
     BlazeBuilder[F]
       .bindHttp(8080, "0.0.0.0")
       .mountService(jsonService, "/")
